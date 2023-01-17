c9d8089995dfdc9db4a2522bb7125a655a25d630
kshivakumar-code <kshivakumar38@gmail.com>
2023-01-17 14:27:15 +0530

    shiva student details changes

--- src/mess/MessAdminProfile.js
+++ src/mess/MessAdminProfile.js
@@ -1,10 +1,45 @@
-import React from "react";
+
+//import React from "react";
 import Base from "../core/Base";
 import Admin from "../assets/images/mess-admin.png";
+import * as XLSX from "xlsx";
+import React, { useState,useEffect } from "react";
 import { Col, Row } from "react-bootstrap";
 
 //mess-details-update, messadminarchives,messreviews, students details in mess
 function MessAdminProfile() {
+  const [item,setItem] = useState([])
+// useEffect (()=>{
+
+  const readExcel = (file)=>{
+    const promise= new Promise((resolve,reject)=>{
+      
+      const fileReader= new FileReader();
+      fileReader.readAsArrayBuffer(file)
+      
+      fileReader.onload=(e)=>{
+        const bufferArray=e.target.result;
+        const wb=XLSX.read(bufferArray,{type:'buffer'});
+        
+        const wsname=wb.SheetNames[0];
+        
+        const ws=wb.Sheets[wsname];
+        const data=XLSX.utils.sheet_to_json(ws);
+        console.log(data)
+        resolve(data);
+        
+      };
+      
+      fileReader.onerror=(error)=>{
+        reject(error);
+      };
+    });
+    promise.then((data)=>{
+      setItem(data)
+      console.log(data)
+    })
+  };
+// },[])
   return (
     <Base title="MESS ADMIN DASHBOARD">
       <div className="card">
@@ -21,10 +56,55 @@ function MessAdminProfile() {
       <div className="card">
         <h3 align="center">Mess Details</h3>
       </div>
+
+
+
       <div className="card">
         <h3 align="center">Student Details</h3>
-        
+        <div>
+          <input type="file" onChange={(e)=>{
+            const file=e.target.files[0];
+            readExcel(file);
+          }}>
+          </input>
+          <table class="table container">
+  <thead>
+    <tr>
+      <th scope="col">MESS</th>
+      <th scope="col">STUDENTNAME</th>
+      <th scope="col">ROLLNO</th>
+      <th scope="col">DUES</th>
+    </tr>
+  </thead>
+  <tbody>
+    {
+      item &&
+      item.map((data)=>{
+        return(<tr key={data.items}>
+      <th scope="row">{data.MESS}</th>
+      <td>{data.STUDENTNAME}</td>
+      <td>{data.ROLLNO}</td>
+      <td>{data.DUES}</td>
+    </tr>)
+
+      })
+      
+    }
+    
+   </tbody>
+  </table>  
+  <center>
+  <button style={{backgroundColor: 'orange', color: 'white', padding: '10px 20px', fontSize: '15px', borderRadius: '10px'}}>
+        Download
+      </button>
+
+  </center>
+  
+</div>
       </div>
+
+
+
       <div className="card">
         <h3 align="center">Update Mess Details</h3>
       </div>
@@ -33,3 +113,4 @@ function MessAdminProfile() {
 }
 
 export default MessAdminProfile;
+
