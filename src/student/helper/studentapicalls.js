4bc37c20a432b913f3e44b68cd15b13d447851b0
Prathamesh Gaikwad <102578693+prathameshg11@users.noreply.github.com>
2023-03-20 12:08:31 +0530

    reated form for messAdmin (#19)
    * added complaints page
    
    * created from for messAdmin
    
    ---------
    
    Co-authored-by: Ambati Sathwik <75169261+AmbatiSathwik@users.noreply.github.com>

--- src/student/helper/studentapicalls.js
+++ src/student/helper/studentapicalls.js
@@ -22,7 +22,6 @@ export const studentDetails = () => {
 };
 
 
-
 export const studentMessDetails = (month, year) => {
   const roll = localStorage.getItem("id").slice(1, -1);
   const token = "Bearer " + localStorage.getItem("jwt").slice(1, -1);
