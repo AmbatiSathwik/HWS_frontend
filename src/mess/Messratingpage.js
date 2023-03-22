ac96f52e5ce26f5ee0637bf667d1f659f23f331c
karthik-siru <karthiksiruvalam@gmail.com>
2023-03-22 20:04:55 +0530

    Mess Ratings Page Done

--- src/mess/Messratingpage.js
+++ src/mess/Messratingpage.js
@@ -1,11 +1,24 @@
 import React, { useState, useEffect } from "react";
 import Base from "../core/Base";
-import Table from 'react-bootstrap/Table';
+import Table from "react-bootstrap/Table";
 import "../styles.css";
 import { FaStar } from "react-icons/fa";
-import {getMessReviewByMessId} from "./helper/messapicalls";
+import { getMessReviewByMessId } from "./helper/messapicalls";
 
-let arr=[];
+let messIdMap = new Map([
+  ["1", "Mess A"],
+  ["2", "Mess B"],
+  ["3", "Mess C"],
+  ["4", "Mess D"],
+  ["5", "Mess E"],
+  ["6", "Mess F"],
+  ["7", "Mess G"],
+  ["8", "Mess IH"],
+  ["9", "Mess MBH"],
+  ["10", "Mess MBH2"],
+  ["11", "Mess Lh"],
+  ["12", "Mess MLH"],
+]);
 
 const getMonthName = (monthNumber) => {
   const date = new Date();
@@ -15,77 +28,116 @@ const getMonthName = (monthNumber) => {
 };
 
 function Messratingspage() {
+  const [arr, setArr] = useState([]);
 
-  console.log(arr);
-
-  const [temp, settemp] = useState(arr);
   useEffect(() => {
-    let i=0;
-     
-    //while(i=12){
-      //console.log(i);
-     // i++;
-    
-      getMessReviewByMessId(1,2023,3).then((data) => {
-       
-      //  console.log(i);
-        console.log(data);
-      if(data.data==null){
-        return;
-      }
-      
-      arr[0].quality=data.data.quality;
-      arr[0].quantity=data.data.quantity;
-      arr[0].catering=data.data.catering;
-      arr[0].cleanlinessAndHygiene=data.data.hyginess;
-      arr[0].punctuality=data.data.punctuality; 
-      settemp(arr);
-    })
-    .catch((err) => {
-      console.log(err);
-    });
-     
-  });
+    let i = 1;
+    while (i <= 12) {
+      getMessReviewByMessId(i, 2023, 3)
+        .then((data) => {
+          if (data != null) {
+            data.data.MessName = messIdMap.get(data.data.id);
+            setArr((state) => [...state, data.data]);
+          }
+        })
+        .catch((err) => {
+          console.log(err);
+        });
 
+      i++;
+    }
+  }, []);
+
+  const loadTableRatings = () => {
+    return (
+      <Table
+        striped
+        bordered
+        hover
+        responsive
+        style={{
+          textAlign: "center",
+          backgroundColor: "white",
+          width: "90%",
+          marginLeft: "5%",
+          marginRight: "5%",
+        }}
+      >
+        <thead>
+          <tr style={{ backgroundColor: "brown", color: "white" }}>
+            <th style={{ textAlign: "center" }}>Sl No.</th>
+            <th style={{ textAlign: "center" }}>Mess Name</th>
+            <th style={{ textAlign: "center" }}>Taste</th>
+            <th style={{ textAlign: "center" }}>Quality</th>
+            <th style={{ textAlign: "center" }}>Quantity</th>
+            <th style={{ textAlign: "center" }}>Catering</th>
+            <th style={{ textAlign: "center" }}>Cleanliness and Hygiene</th>
+            <th style={{ textAlign: "center" }}>Punctuality</th>
+          </tr>
+        </thead>
+        {/* {arr.sort((a, b) => a.id.localeCompare(b.id))} */}
+        {arr.map((x, index) => (
+          <tbody key={x.id}>
+            <tr style={{ textAlign: "center" }}>
+              <td>{index + 1}</td>
+              <td>{x.MessName ? x.MessName : messIdMap.get(x.id)}</td>
+              <td>
+                {x.taste ? x.taste : "NA"}
+                <FaStar style={{ color: "gold" }} />
+              </td>
+
+              <td>
+                {x.quality ? x.quality : "NA"}
+                <FaStar style={{ color: "gold" }} />
+              </td>
+              <td>
+                {x.quantity ? x.quantity : "NA"}
+                <FaStar style={{ color: "gold" }} />
+              </td>
+              <td>
+                {x.catering ? x.catering : "NA"}
+                <FaStar style={{ color: "gold" }} />
+              </td>
+              <td>
+                {x.hyginess ? x.hyginess : "NA"}
+                <FaStar style={{ color: "gold" }} />
+              </td>
+              <td>
+                {x.punctuality ? x.punctuality : "NA"}
+                <FaStar style={{ color: "gold" }} />
+              </td>
+            </tr>
+          </tbody>
+        ))}
+      </Table>
+    );
+  };
 
   return (
     <Base title="">
-
-    <div >
-    <h1 className="m-3" > <center>Mess Ratings<FaStar style={{color:"gold"}}/><FaStar style={{color:"gold"}}/></center></h1>{"\n"}
-    <p className=""><center>Following scores are based on the feedback received from students and are out of 5<FaStar style={{color:"gold"}}/>.</center></p>
-<div style={{width:"100%",justifyContent:"center"}} className="px-5">
-   
-    <Table striped bordered hover responsive style={{textAlign:"center",backgroundColor:"white",width:"90%",marginLeft:"5%",marginRight:"5%"}} >
-      <thead>
-        <tr style={{backgroundColor:"brown",color:"white"}}>
-          <th style={{textAlign:"center"}}>Mess Name</th>
-          <th style={{textAlign:"center"}}>Quality</th>
-          <th style={{textAlign:"center"}}>Quantity</th>
-          <th style={{textAlign:"center"}}>Catering</th>
-          <th style={{textAlign:"center"}}>Cleanliness and Hygiene</th>
-          <th style={{textAlign:"center"}}>Punctuality</th>
-        </tr>
-      </thead>
-
-      {arr.map((x) => (
-        <tbody >
-        <tr style={{textAlign:"center"}}>
-
-          <td >{x.MessName}</td>
-          <td >{x.Quality}<FaStar style={{color:"gold"}}/></td>
-          <td >{x.Quantity}<FaStar style={{color:"gold"}}/></td>
-          <td >{x.Catering}<FaStar style={{color:"gold"}}/></td>
-          <td >{x.CleanlinessAndHygiene}<FaStar style={{color:"gold"}}/></td>
-          <td >{x.Punctuality}<FaStar style={{color:"gold"}}/></td>
-
-        </tr>
-      </tbody>
-      ))}
-      
-    </Table>
-    </div>
-    </div>
+      <div>
+        <h1 className="m-3">
+          {" "}
+          <center>
+            Mess Ratings
+            <FaStar style={{ color: "gold" }} />
+            <FaStar style={{ color: "gold" }} />
+          </center>
+        </h1>
+        <br />
+        <div className="">
+          <center>
+            Following scores are based on the feedback received from students
+            and are out of 5<FaStar style={{ color: "gold" }} />.
+          </center>
+        </div>
+        <div
+          style={{ width: "100%", justifyContent: "center" }}
+          className="px-5"
+        >
+          {loadTableRatings()}
+        </div>
+      </div>
     </Base>
   );
 }
